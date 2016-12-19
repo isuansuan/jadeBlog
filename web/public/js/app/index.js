13a0d30b6cd32420efba0889d9f8f51e3da079c4
zhengjinwei123 <2538698032@qq.com>
2016-12-19 15:37:04 +0800

    添加 各种第三方模块 requirejs boostrap lodash underscore

---
+++ web/public/js/app/index.js
@@ -0,0 +1,12 @@
+define(function (require, exports, module) {
+    var $ = require("jquery");
+    var _ = require("lodash");
+
+    $(document).ready(function () {
+        $('body').css({background: "red"});
+    });
+
+    //require(["jquery", "lodash"], function ($, _) {
+    //
+    //});
+});
---
+++ web/public/js/main.js
@@ -0,0 +1,8 @@
+require.config({
+    paths: {
+        "jquery": ["http://libs.baidu.com/jquery/2.0.3/jquery", "lib/jquery/jquery3.1.1"],
+        "bootstrap": ["lib/bootstrap/js/bootstrap.min"],
+        "lodash": ["lib/lodash-4.17.2/lodash"]
+    }
+});
+
