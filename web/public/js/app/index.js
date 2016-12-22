5cb87c046a64e7fe8a25b8ea0c0aba390cf666dd
zhengjinwei123 <2538698032@qq.com>
2016-12-22 23:21:06 +0800

    add blog editor

--- web/public/js/app/index.js
+++ web/public/js/app/index.js
@@ -2,12 +2,17 @@ define(function (require, exports, module) {
     var $ = require("jquery");
     var _ = require("lodash");
     var BS = require("bootstrap");
+    var summernote = require("summernote");
 
     $(document).ready(function () {
         //$('body').css({background: "#808080"});\
         $('#idCarousel').carousel({
             interval: 1000
         });
+
+        $('#summernote').summernote({ lang: 'zh-CN' });
+        $(".modal").hide();
+
     });
 
     //require(["jquery", "lodash"], function ($, _) {
