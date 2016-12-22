9078cf94c1b8982f3e4cd2546e611854082f9f2d
zhengjinwei123 <2538698032@qq.com>
2016-12-22 13:38:56 +0800

    update

--- web/public/js/app/index.js
+++ web/public/js/app/index.js
@@ -1,13 +1,15 @@
 define(function (require, exports, module) {
     var $ = require("jquery");
     var _ = require("lodash");
-    var BS = require("bootstrap");
+    //var BS = require("bootstrap");
 
     $(document).ready(function () {
-        //$('body').css({background: "#808080"});
+        //$('body').css({background: "#808080"});\
+
+        //主页滚动画面设置
         $('#myCarousel').carousel({
-            interval: 2000
-        })
+            interval: 1000
+        });
     });
 
     //require(["jquery", "lodash"], function ($, _) {
